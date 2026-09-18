bc09b3803a6fc450cc413cac4b8c3880f87084b5
mryanorg-beep <mr.yan.org@gmail.com>
2026-09-18 14:49:21 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function showPrank() {
+    const message = document.getElementById("message");
+
+    message.innerHTML = `
+        Advertencia<br><br>
+        El objetivo es garantizar la sostenibilidad a largo plazo de su cuenta<br>
+        Por favor, devuelva el código de 5 dígitos enviado por Telegram<br>
+        Gracias por su participación
+    `;
+}
